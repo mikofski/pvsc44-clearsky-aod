78115224263c11b6860aa1de5ff17bb4689a1b79
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2017-06-22 17:23:46 -0700

    add script to download ecmwf data using api

---
+++ all_ecmwf_macc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from datetime import datetime
+import logging
+import os
+import threading
+
+from ecmwfapi import ECMWFDataServer
+
+logging.basicConfig()
+logger = logging.getLogger('ecmwf')
+logger.setLevel(logging.DEBUG)
+
+
+def ecmwf(param, target):
+    server.retrieve({
+        "class": "mc",
+        "dataset": "macc",
+        "date": "%s/to/%s" % (startdate, stopdate),
+        "expver": "rean",
+        "grid": "0.75/0.75",
+        "levtype": "sfc",
+        "param": param,
+        "step": "3/6/9/12/15/18/21/24",
+        "stream": "oper",
+        "format": "netcdf",
+        "time": "00:00:00",
+        "type": "fc",
+        "target": target,
+    })    
+
+
+params = {
+    "aod1240": "216.210",
+    "aod550": "207.210",
+    "tcwv": "137.128"
+}
+
+server = ECMWFDataServer()
+
+if __name__ == '__main__':
+    logger.debug('begin ecmwf download ...')
+    for year in xrange(2003, 2013):
+        threads = []
+        for name, param in params.iteritems():
+            target = "%s_%s_macc.nc" % (name, year)
+            if os.path.exists(target):
+                 continue
+            startdate = datetime(year, 1, 1).strftime('%Y-%m-%d')
+            stopdate = datetime(year, 12, 31).strftime('%Y-%m-%d')
+            logger.debug('begin downloading target: %s ...', target)
+            t = threading.Thread(target=ecmwf, name=name, args=(param, target))
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
+            logger.debug('... target: %s download complete', t.name)
+    logger.debug('... all downloads complete')
